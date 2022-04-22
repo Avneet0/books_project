7fd5298ba93275dd9d536624aecfd567b16c57d8
Avneet0 <avikalra005@gmail.com>
2022-04-22 11:59:38 +0530

    second commit

--- books/views.py
+++ books/views.py
@@ -3,16 +3,26 @@ from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMix
 # Create your views here.
 from django.views.generic import ListView, DetailView
 from .models import Books
-
-class BookListView(LoginRequiredMixin,ListView):
+from django.db.models import Q
+class BookListView(ListView):
     model = Books
     context_object_name = 'book_list'
     template_name = 'books/book_list.html'
     login_url = 'account_login'
 
-class BookDetailView(LoginRequiredMixin,PermissionRequiredMixin,DetailView):
+class BookDetailView(DetailView):
     model = Books
     context_object_name = 'book'
     template_name = 'books/book_detail.html'
     login_url = 'account_login'
     permission_required = 'books.special_status'
+
+class SearchResultsListView(ListView): # new
+    model = Books
+    context_object_name = 'book_list'
+    template_name = 'books/search_results.html'
+
+    def get_queryset(self):
+        return Books.objects.filter(
+            Q(title__icontains='money') | Q(title__icontains='api')
+        )
